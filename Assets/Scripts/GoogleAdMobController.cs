fb166cec18d241a46755b965fcd82e4ee768776a
Faruk <farukkayaduman@outlook.com>
2023-05-09 15:36:03 +0300

    Ready to build v0.4.0

--- Assets/Scripts/GoogleAdMobController.cs
+++ Assets/Scripts/GoogleAdMobController.cs
@@ -50,7 +50,7 @@ namespace AdMobController
                 _interstitialAd = null;
             }
 
-            Debug.Log("Loading the interstitial ad.");
+            // Debug.Log("Loading the interstitial ad.");
 
             // create our request used to load the ad.
             var adRequest = new AdRequest.Builder().Build();
@@ -62,13 +62,11 @@ namespace AdMobController
                     // if error is not null, the load request failed.
                     if (error != null || ad == null)
                     {
-                        Debug.LogError("interstitial ad failed to load an ad " +
-                                       "with error : " + error);
+                        // Debug.LogError("interstitial ad failed to load an ad " + "with error : " + error);
                         return;
                     }
 
-                    Debug.Log("Interstitial ad loaded with response : "
-                              + ad.GetResponseInfo());
+                    // Debug.Log("Interstitial ad loaded with response : " + ad.GetResponseInfo());
 
                     _interstitialAd = ad;
                     RegisterEventHandlers(_interstitialAd);
@@ -82,18 +80,18 @@ namespace AdMobController
         {
             if (_interstitialAdTimer < _adCooldownSecond)
             {
-                Debug.LogError("On cooldown for interstitial ad.");
+                // Debug.LogError("On cooldown for interstitial ad.");
                 return;
             }
 
             if (_interstitialAd != null && _interstitialAd.CanShowAd())
             {
-                Debug.Log("Showing interstitial ad.");
+                // Debug.Log("Showing interstitial ad.");
                 _interstitialAd.Show();
             }
             else
             {
-                Debug.LogError("Interstitial ad is not ready yet.");
+                // Debug.LogError("Interstitial ad is not ready yet.");
             }
         }
 
@@ -102,36 +100,33 @@ namespace AdMobController
             // Raised when the ad is estimated to have earned money.
             ad.OnAdPaid += (AdValue adValue) =>
             {
-                Debug.Log(String.Format("Interstitial ad paid {0} {1}.",
-                    adValue.Value,
-                    adValue.CurrencyCode));
+                // Debug.Log(String.Format("Interstitial ad paid {0} {1}.", adValue.Value, adValue.CurrencyCode));
             };
             // Raised when an impression is recorded for an ad.
             ad.OnAdImpressionRecorded += () =>
             {
-                Debug.Log("Interstitial ad recorded an impression.");
+                // Debug.Log("Interstitial ad recorded an impression.");
             };
             // Raised when a click is recorded for an ad.
             ad.OnAdClicked += () =>
             {
-                Debug.Log("Interstitial ad was clicked.");
+                // Debug.Log("Interstitial ad was clicked.");
             };
             // Raised when an ad opened full screen content.
             ad.OnAdFullScreenContentOpened += () =>
             {
-                Debug.Log("Interstitial ad full screen content opened.");
+                // Debug.Log("Interstitial ad full screen content opened.");
             };
             // Raised when the ad closed full screen content.
             ad.OnAdFullScreenContentClosed += () =>
             {
-                Debug.Log("Interstitial ad full screen content closed.");
+                // Debug.Log("Interstitial ad full screen content closed.");
                 LoadInterstitialAd();
             };
             // Raised when the ad failed to open full screen content.
             ad.OnAdFullScreenContentFailed += (AdError error) =>
             {
-                Debug.LogError("Interstitial ad failed to open full screen content " +
-                               "with error : " + error);
+                // Debug.LogError("Interstitial ad failed to open full screen content with error : " + error);
                 LoadInterstitialAd();
             };
         }
